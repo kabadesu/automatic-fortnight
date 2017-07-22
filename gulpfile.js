95ea9f331dc00561cfab035b3cd01f42bd475f62
Murray Nuttall <murray.nuttall@gmail.com>
2017-07-22 14:27:46 +1200

    Added svgs, some ui elements, and some build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,78 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
+var server = require('gulp-server-livereload');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cleanCSS = require('gulp-clean-css');
-var sassLint = require('gulp-sass-lint');
+var svgstore = require('gulp-svgstore');
+var svgmin = require('gulp-svgmin');
+var cheerio = require('gulp-cheerio');
 
 var errorHandler = function (e) {
-	console.log(e);
-}
+    console.log(e);
+};
 
-gulp.task('compile-sass', ['lint-sass'], function () {
-	var plugins = [
-		autoprefixer({ browsers: ['last 2 versions'] })
-	];
-	return gulp.src('sass/style.scss')
-	.pipe(sass({
-		includePaths: 'node_modules/normalize-scss/sass'
-	}))
-	.on('error', errorHandler)
-	.pipe(postcss(plugins))
-	.pipe(cleanCSS())
-	.pipe(gulp.dest('css/'))
+var path = {
+    app: 'dist/',
+    sassMain: 'src/sass/style.scss',
+    sassGlob: 'src/sass/**/*.scss',
+    svgGlob: 'src/images/svgs/**/*.svg',
+    distCSS: 'dist/css/',
+    distImages: 'dist/images/'
+};
+
+gulp.task('webserver', function () {
+    gulp.src(path.app)
+        .pipe(server({
+            livereload: true,
+            open: true
+        }));
 });
 
-gulp.task('build', ['compile-sass']);
+gulp.task('sprites', function () {
+    return gulp.src(path.svgGlob)
+        .pipe(svgmin({
+            plugins: [{
+                removeTitle: false,
+            }]
+        }))
+        .pipe(cheerio({
+            run: function ($) {
+                var black = ['#000', '#000000'];
+                $('[fill]').each(function() {
+                    var $fill = $(this).attr('fill');
+                    if (black.indexOf($fill) !== -1) {
+                        $(this).removeAttr('fill')
+                    }
+                });
+            },
+            parserOptions: { xmlMode: true }
+        }))
+        .pipe(svgstore())
+        .pipe(rename('sprite.svg'))
+        .pipe(gulp.dest(path.distImages))
+});
+
+gulp.task('compile-sass', function () {
+    return gulp.src(path.sassMain)
+        .pipe(sass({
+            includePaths: [
+                'node_modules/normalize-scss/sass'
+            ]
+        }))
+        .on('error', errorHandler)
+        .pipe(postcss([
+            autoprefixer({browsers: ['last 2 versions']})
+        ]))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(path.distCSS))
+});
+
+gulp.task('build', ['compile-sass', 'sprites']);
 
 gulp.task('watch', ['build'], function () {
-	gulp.watch('sass/**/*.scss', ['compile-sass']);
-})
+    gulp.watch(path.sassGlob, ['compile-sass']);
+});
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch', 'webserver']);
